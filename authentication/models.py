33cb3d831e83fcc7cd7bd00ac2e909f309dedef3
konga <kongadan68@gmail.com>
2023-10-27 23:03:25 +0200

    Personnalisation de modele User

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
-# Create your models here.
+class User(AbstractUser):
+    CREATOR='CREATOR'
+    SUBSCRIBER='SUBSCRIBER'
+    ROLE_CHOICES=((CREATOR,'Createur'),(SUBSCRIBER,'Abonne'))
+    profile_photo=models.ImageField(verbose_name='Profil photo')
+    role=models.CharField(max_length=68,choices=ROLE_CHOICES,verbose_name='Role')
